deb20b7c8fa96b84d5615965d63a44a049f6f47e
Ben-Peters <49176877+Ben-Peters@users.noreply.github.com>
2022-11-21 14:32:25 -0500

    finish vasic headless client

--- source/HeadlessClient.cpp
+++ source/HeadlessClient.cpp
@@ -2,19 +2,28 @@
 #include "NetworkUtils.h"
 #include "LatentNetwork.h"
 #include <G3D/G3D.h>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 ENetHost* localHost;
 ENetAddress reliableServerAddress;
 ENetAddress unreliableServerAddress;
-String serverAddressStr = "127.0.0.1";
+String serverAddressStr = "localhost";
 int serverPort = 18821;
 ENetPeer* serverPeer;
 ENetSocket unreliableSocket;
 bool enetConnected, socketConnected;
 bool runningTrial;
-GUniqueID playerGUID = GUniqueID::create();
+GUniqueID playerGUID;
 int networkLatency = 100;
 int localFrameNumber = 0;
+int framerate = 60;
+String csvFile = "positions.csv";
+std::fstream file;
+
+G3D_START_AT_MAIN();
 
 void connectToServer() {
 	if (enet_initialize() != 0) {
@@ -22,21 +31,23 @@ void connectToServer() {
 		throw std::runtime_error("Could not initialize ENet networking");
 
 	}
+	playerGUID = GUniqueID::create();
 
 	localHost = enet_host_create(NULL, 1, 2, 0, 0); // create a host on an arbitrary port which we use to contact the server
-	if (localHost == nullptr)
+	if (localHost == NULL)
 	{
 		throw std::runtime_error("Could not create a local host for the server to connect to");
 	}
 	enet_address_set_host(&reliableServerAddress, serverAddressStr.c_str());
 	reliableServerAddress.port = serverPort;
 	serverPeer = enet_host_connect(localHost, &reliableServerAddress, 2, 0);		//setup a peer with the server
-	if (serverPeer == nullptr)
+	if (serverPeer == NULL)
 	{
 		throw std::runtime_error("Could not create a connection to the server");
 	}
-	logPrintf("created a peer with the server at %s:%d (the connection may not have been accepeted by the server)", serverAddressStr, serverPort);
+	debugPrintf("created a peer with the server at %s:%d (the connection may not have been accepeted by the server)", serverAddressStr, serverPort);
 
+	enet_address_set_host(&unreliableServerAddress, serverAddressStr.c_str());
 	unreliableServerAddress.port = serverPort + 1;
 	unreliableSocket = enet_socket_create(ENET_SOCKET_TYPE_DATAGRAM);
 	enet_socket_set_option(unreliableSocket, ENET_SOCKOPT_NONBLOCK, 1); //Set socket to non-blocking
@@ -47,12 +58,61 @@ void connectToServer() {
 }
 
 void sendLocation() {
-	//TODO
+	std::string line;
+	if (file.is_open()) {
+		if (std::getline(file, line)) {
+			std::stringstream strstream = std::stringstream(line);
+			std::string word;
+			std::getline(strstream, word, ',');
+			float pos_az, pos_el, pos_x, pos_y, pos_z;
+			pos_az = std::stof(word);
+			std::getline(strstream, word, ',');
+			pos_el = std::stof(word);
+			std::getline(strstream, word, ',');
+			pos_x = std::stof(word);
+			std::getline(strstream, word, ',');
+			pos_y = std::stof(word);
+			std::getline(strstream, word, ',');
+			pos_z = std::stof(word);
+
+			// TODO: make this include rotation information
+			CFrame frame = G3D::CFrame(Point3(pos_x, pos_y, pos_z));
+			
+			shared_ptr<BatchEntityUpdatePacket> updatePacket = GenericPacket::createUnreliable<BatchEntityUpdatePacket>(&unreliableSocket, &unreliableServerAddress);
+			BatchEntityUpdatePacket::EntityUpdate update = BatchEntityUpdatePacket::EntityUpdate();
+			update.frame = frame;
+			update.name = playerGUID.toString16();
+			
+			// TODO: make this use a real frame number
+			updatePacket->populate(0, Array<BatchEntityUpdatePacket::EntityUpdate>(update), BatchEntityUpdatePacket::NetworkUpdateType::REPLACE_FRAME);
+			NetworkUtils::send(updatePacket);
+		}
+		else {
+			debugPrintf("Reached the end of the file quitting this client now\n");
+			enet_peer_disconnect(serverPeer, NULL);
+			enet_host_service(localHost, nullptr, NULL);
+			Sleep(1000);
+			exit(0);
+		}
+	}
+	else {
+		debugPrintf("ERROR: File is not open!\n");
+		throw std::runtime_error("CSV file was not open");
+	}
 }
 
 int main(int argc, const char* argv[]) {
+	std::chrono::nanoseconds interframeTime = std::chrono::nanoseconds(1000000000) / framerate;
+	auto nextUpdate = std::chrono::high_resolution_clock::now() + interframeTime;
+	file = std::fstream(csvFile.c_str(), std::ios::in);
+
 	connectToServer();
 	while (true) {
+		if (!socketConnected) {
+			// If we haven't received a response yet send another handshake packet
+			shared_ptr<HandshakePacket> outPacket = GenericPacket::createUnreliable<HandshakePacket>(&unreliableSocket, &unreliableServerAddress);
+			outPacket->send();
+		}
 		/* Handle all incoming packets (ignore most of them) */
 		shared_ptr<GenericPacket> inPacket = NetworkUtils::receivePacket(localHost, &unreliableSocket);
 		while (inPacket != nullptr) {
@@ -132,14 +192,18 @@ int main(int argc, const char* argv[]) {
 					break;
 				}
 				}
-				shared_ptr<GenericPacket> inPacket = NetworkUtils::receivePacket(localHost, &unreliableSocket);
 			}
+			inPacket = NetworkUtils::receivePacket(localHost, &unreliableSocket);
 		}
 
 		/* If we are running a trial and there has been the right amount of time since the last frame send the next frame */
-		if (runningTrial) {
+		auto now = std::chrono::high_resolution_clock::now();
+		if (runningTrial && now >= nextUpdate) {
 			sendLocation();
-			// TODO: All of this needs to be implemented
+			nextUpdate = std::chrono::high_resolution_clock::now() + interframeTime;
+		}
+		else {
+			std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::nanoseconds(500));
 		}
 	}
 }
